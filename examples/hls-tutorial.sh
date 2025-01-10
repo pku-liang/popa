6e21cc12d3e0a74fe19980dc32736f2beb0e1509
haoxiaochen <71343933+haoxiaochen@users.noreply.github.com>
2025-01-10 20:45:10 +0800

    Update hls-tutorial.sh

--- examples/hls-tutorial.sh
+++ examples/hls-tutorial.sh
@@ -23,7 +23,7 @@ run_hector() {
     hestia_run=$(readlink -f $HESTIA_DIR/target/release/hestia)
     pushd $HECTOR_DIR >/dev/null
     ./build/bin/hector-opt $1 --canonicalize --hls-unroll --affine-loop-normalize --canonicalize --new-array-partition --canonicalize --remove-access=mode=aggressive --lower-affine \
-        --convert-input="top-function=_kernel_C_s0_run_on_device resource=./examples/resource_dynamatic.json" --dump-scf --scf-to-tor="pipeline" --schedule-tor --split-schedule --dump-tor="json=tor.json" 2>/dev/null
+        --convert-input="top-function=_kernel_C_s0_run_on_device resource=./examples/resource_dynamatic.json" --dump-scf --scf-to-tor="pipeline" --schedule-tor --split-schedule --dump-tor="json=tor.json" &>/dev/null
     $hestia_run tor.tcl
     popd >/dev/null
 }
