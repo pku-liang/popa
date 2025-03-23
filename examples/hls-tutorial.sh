b626850aed763feeda4a7c9b221c717e66a29cc4
xiaochen.hao <xiaochen.hao@stu.pku.edu.cn>
2025-03-23 15:26:31 +0800

    Update tutorial script

--- examples/hls-tutorial.sh
+++ examples/hls-tutorial.sh
@@ -6,6 +6,8 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+HECTOR_DIR=${HECTOR_DIR:=~/repos/hector}
+
 run_popa() {
     if [[ ! -e "matrix_multiply" ]]; then
         g++ matrix_multiply.cpp -g -I../install/include -L../install/lib -lHalide -std=c++17 -o matrix_multiply
@@ -14,21 +16,24 @@ run_popa() {
 }
 
 run_hector() {
+    pushd $HECTOR_DIR >/dev/null
     which hector-opt
     which hestia
     hector-opt $1 --canonicalize --hls-unroll --affine-loop-normalize --canonicalize --new-array-partition --canonicalize --remove-access=mode=aggressive --lower-affine \
         --convert-input="top-function=_kernel_C_s0_run_on_device resource=./examples/resource_dynamatic.json" --dump-scf --scf-to-tor="pipeline" --schedule-tor --split-schedule --dump-tor="json=tor.json" &>/dev/null
     hestia tor.tcl
+    popd >/dev/null
 }
 
 if [[ "$1" == "generate" ]]; then
     run_popa $2
 elif [[ "$1" == "run" ]]; then
-    if [[ ! -e "$POPA_DIR/examples/SCF_$2.mlir" ]]; then
+    if [[ ! -e "SCF_$2.mlir" ]]; then
         echo "Please first generate the MLIR file SCF_$2.mlir"
         exit 1
     fi
-    run_hector SCF_$2.mlir
+    cp SCF_$2.mlir $HECTOR_DIR/examples/popa/
+    run_hector examples/popa/SCF_$2.mlir
 else
     echo "Invalid command"
 fi
